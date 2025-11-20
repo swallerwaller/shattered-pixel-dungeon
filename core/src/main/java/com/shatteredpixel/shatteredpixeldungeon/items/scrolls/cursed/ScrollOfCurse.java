52278c8a57d153b522310b9ae5cd1c48d71ed831
[alice <alicemcgee97@gmail.com>
2025-11-19 21:14:39 -0500

    Worked on Scroll of Curse Code

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/cursed/ScrollOfCurse.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/cursed/ScrollOfCurse.java
@@ -21,14 +21,45 @@ import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Amok;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.utils.PathFinder;
+
 
 public class ScrollOfCurse extends CursedScroll {
 
     {
         icon = ItemSpriteSheet.Icons.SCROLL_CURSE;
-        preferredBag = Belongings.Backpack.class;
+        //preferredBag = Belongings.Backpack.class;
     }
+
+    @Override
+    public void doRead() {
+        Mob mob = null; //creates a mob category that we are looking for
+        for (int i : PathFinder.NEIGHBOURS8){ //runs a loop testing each square adjacent to the player
+            if (Actor.findChar(curUser.pos+i) instanceof Mob){ //look for the mob
+                mob = (Mob) Actor.findChar(curUser.pos+i); //if we dont find the mob
+            }
+        }
+        if (mob != null){ //if we do find the mob
+            identify();//identify the scroll
+            Sample.INSTANCE.play( Assets.Sounds.READ );
+            readAnimation();
+            detach(curUser.belongings.backpack);
+
+    } else {
+            //we will be prompted to choose an item to curse
+
+        }
 
 }
